2ba4168302430ff322eb9e05bbe1f6f09d941976
Gagandeep Ramgiri <84025135+GagandeepRamgiri@users.noreply.github.com>
2022-09-22 23:24:09 +0530

    Update CustomerController.java
    controller updated

--- src/main/java/com/sony/controller/CustomerController.java
+++ src/main/java/com/sony/controller/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.sony.entity.Customer;
 import com.sony.entity.CustomerList;
